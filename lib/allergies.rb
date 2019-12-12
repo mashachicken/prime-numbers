cd6e4add915667fa11d366ba91908be66805e918
Student <example@example.com>
2019-12-12 14:06:38 -0800

    allergies now functions properly

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -1,13 +1,43 @@
 class Allergies
   def initialize ()
-    @allergies = {1 => 'eggs', 2 => 'peanuts', 4 => 'shellfish', 8 => 'strawberries', 16 => 'tomatoes', 32 => 'chocolate', 64 => 'pollen', 128 => 'cats'}
+    @allergies =
+    {
+      1 => 'eggs',
+      2 => 'peanuts',
+      4 => 'shellfish',
+      8 => 'strawberries',
+      16 => 'tomatoes',
+      32 => 'chocolate',
+      64 => 'pollen',
+      128 => 'cats'
+    }
   end
-  def get_allergies(number)
-    array = []
 
-    if (@allergies.invert.values).include?(number)
-      return [@allergies.fetch(number)]
+  def get_allergies(input)
+    array = []
+    number_array = [128, 64, 32, 16, 8, 4, 2]
+
+    # This finds an exact match and returns the string value
+    if (@allergies.invert.values).include?(input)
+      return [@allergies.fetch(input)]
     end
+
+    # If the number is odd give them eggs
+    if input.odd?()
+      array.push(@allergies.fetch(1))
+      input -= 1
+    end
+
+    # This uses number_array to iterate through the possible values and pushs the strings into the return array
+    x = 0
+    while x < 7
+      if (input - number_array[x]) >= 0
+        input -= number_array[x]
+        array.push(@allergies.fetch(number_array[x]))
+      end
+      x += 1
+    end
+    return array
 
   end
 end
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -5,8 +5,24 @@ require('allergies.rb')
 
 describe('Allergies#get_allergies') do
 
-  it("Should return the value of an allergy") do
+  it("Should return the value of an exact allergy") do
     new_allergy = Allergies.new()
     expect(new_allergy.get_allergies(128)).to(eq(['cats']))
+  end
+  it("Should add eggs if the number is odd") do
+    new_allergy = Allergies.new()
+    expect(new_allergy.get_allergies(1)).to(eq(['eggs']))
+  end
+  it("Should return an array with multiple allergies: 'eggs' 'cats'") do
+    new_allergy = Allergies.new()
+    expect(new_allergy.get_allergies(129)).to(eq(['eggs', 'cats']))
+  end
+  it("Should return an array with multiple allergies: 'eggs' 'strawberries'") do
+    new_allergy = Allergies.new()
+    expect(new_allergy.get_allergies(9)).to(eq(['eggs', 'strawberries']))
+  end
+  it("SShould return an array with multiple allergies: 'strawberries' 'peanuts'") do
+    new_allergy = Allergies.new()
+    expect(new_allergy.get_allergies(10)).to(eq(['strawberries', 'peanuts']))
   end
 end
